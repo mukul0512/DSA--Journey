47ea699b9720065ca5ee74b8ebe1ae396c6033f2
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-08-29 16:55:04 +0530

    count neg num from array

--- countNegative.js
+++ countNegative.js
@@ -1,11 +1,9 @@
 // write a function that returns the count of negative numbers in an array. where given arr = [2, -9, 17, 0, 1, -10, -4, 8]
 let arr = [2, -9, 17, 0, 1, -10, -4, 8];
 let count = 0;
-let negNum = [];
 function countNegativeNum(arr) {
     for(let i = 0; i < arr.length; i++) {
         if(arr[i] < 0) {
-            negNum = arr[i];
             count++;
         }
     }
