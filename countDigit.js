2c977bbe1da0e349c9d3ff78a61f4fafb4ab8843
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-09-06 11:09:02 +0530

    optimized sol. of count digits with edge cases handled

--- countDigit.js
+++ countDigit.js
@@ -1,8 +1,15 @@
 // Write a function that return the count of digits in a number, n = 259
-let n = 259;
+// Edge cases -> 
+// 1. if n = 0;
+// 2. if n = -298;
+let n = -259;
 function countDigits(n) {
+    if (n == 0) {
+        return 1;
+    }
+    n = Math.abs(n); // convert -ve num to +ve num, abs stands for absolute for handling -ve numbers.
     let count = 0;
-    while(n > 0){
+    while (n > 0) {
         n = Math.floor(n / 10);
         count++;
     }
