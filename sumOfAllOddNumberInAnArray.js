dcfbf86c6047738e9bab90af3d0758ff4f3a142e
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-09-12 17:53:36 +0530

    solved sum of all odd number in an array using recursion

---
+++ sumOfAllOddNumberInAnArray.js
@@ -0,0 +1,21 @@
+let arr = [5, 3, 2, 0, 1];
+let n = arr.length;
+function sumOfOddNumber(n) {
+    let isOdd = arr[n] % 2 !== 0;
+    if (n == 0) {
+        if (isOdd) {
+            return arr[n];
+        }
+        else {
+            return 0;
+        }
+    }
+    if (isOdd) {
+        return arr[n] + sumOfOddNumber(n - 1)
+    }
+    else {
+        return 0 + sumOfOddNumber(n - 1);
+    }
+}
+let res = sumOfOddNumber(n - 1);
+console.log(res);
