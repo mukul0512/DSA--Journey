948a4926ae9f18323b748ade615da000fbd7312f
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-09-16 11:54:17 +0530

    solved bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,20 @@
+let arr = [5, 4, 9, 15, 7, 1, 0];
+let n = arr.length;
+function bubbleSort(arr) {
+    for (let i = 0; i < n - 1; i++) {
+        let isSwapped = false;
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                isSwapped = true;
+            }
+        }
+        if (!isSwapped) break;
+    }
+    return arr;
+}
+
+let res = bubbleSort(arr);
+console.log(res);
